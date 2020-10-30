d3e47ee6208d64ccf6c812159b1ede09b0c1055c
kysechan <kylechang96@gmail.com>
2020-10-30 16:56:02 -0700

    testing App.js using text box

--- nba-db/src/App.js
+++ nba-db/src/App.js
@@ -2,30 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import React, {Component} from 'react';
 
-function App() {
-  ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-  );
-}
-
-
-
-
-
-// class EssayForm extends React.Component {
-//   render() {
-//     return (
-//       <form>
-//         <label>
-//           Name:
-//           <input type="text" name="name" />
-//         </label>
-//         <input type="submit" value="Submit" />
-//       </form>
-//     );
-//   }
+// function App() {
+//   ReactDOM.render(
+//     <h1>Hello, world!</h1>,
+//     document.getElementById('root')
+//   );
 // }
+
+
+
+
+
+class EssayForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
 
 
 export default App;
