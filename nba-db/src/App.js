2cc5a6ffd3fef2e15717a475baff2907eed31101
kysechan <kylechang96@gmail.com>
2020-10-30 19:01:50 -0700

    testing App.js using text box

--- nba-db/src/App.js
+++ nba-db/src/App.js
@@ -1,31 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Center from 'react-center';
+import { Redirect } from 'react-router-dom';
 
 class Simpletextarea extends Component {
+
   constructor() {
     super();
     this.state = {
       name: "React"
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    console.log(event.target.value)
+    this.setState({value: event.target.value});
   }
+
+  handleSubmit(event) {
+    fetch('http://0.0.0.0:8080/api/player/basic?player='+this.state.value)
+    .then(response => {
+      return response.json()
+    })
+    .then(response => {console.log(response); alert(JSON.stringify(response))})
+    .catch(error => {
+      console.error('Error:', error);
+      alert('Could not find player: ' + this.state.value)
+    })
+    event.preventDefault()
+  }
+
+  
 
   render() {
     return (
       <Center>
-        <div>
-          <label>Enter value : </label>
-          <input type="textarea" 
-            name="textValue"
-            onChange={this.handleChange}
-          />
-        </div>
+          <div>
+            <form onSubmit={this.handleSubmit}>
+            <label>Player: </label>
+            <input type="textarea" 
+              name="player"
+              onChange={this.handleChange}
+            />
+            <input type="submit" value="Submit"/>
+            </form>
+          </div>
       </Center>
     );
   }
