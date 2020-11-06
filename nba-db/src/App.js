d00a6730b5e340466e12b55b0a49d0a77413d44f
kysechan <kylechang96@gmail.com>
2020-11-05 19:56:05 -0800

    fixing API url's to have https

--- nba-db/src/App.js
+++ nba-db/src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
 
   // Get request API player endpoint
   search_player(event) {
-    fetch("http://164.90.149.249:8080/api/player/basic?player=" + this.state.value)
+    fetch("https://164.90.149.249:8080/api/player/basic?player=" + this.state.value)
       .then((response) => response.json())
       .then((response) => {
         console.log(response)
@@ -83,7 +83,7 @@ class App extends Component {
 
   // Get request API player endpoint
   search_team(event) {
-    fetch("http://164.90.149.249:8080/api/teams/basic?team=" + this.state.value)
+    fetch("https://164.90.149.249:8080/api/teams/basic?team=" + this.state.value)
       .then((response) => response.json())
       .then((response) => {
         console.log(response)
